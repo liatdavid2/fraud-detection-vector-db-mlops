25ec480d10b519fb20c706ee2ca1462b2b74bc2f
liatdavid2 <liatdavid2@gmail.com>
2026-06-19 11:43:46 +0300

    Update make_sample_and_train.cmd

--- scripts/make_sample_and_train.cmd
+++ scripts/make_sample_and_train.cmd
@@ -2,4 +2,4 @@
 call .venv\Scripts\activate
 python -m fraud_vector_db_mlops.data --make-sample
 python -m fraud_vector_db_mlops.validation
-python -m fraud_vector_db_mlops.train --skip-milvus
+python -m fraud_vector_db_mlops.train
